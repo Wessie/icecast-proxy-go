9048c171eac4f9658e1788230b8982f219bf2831
Wessie <wessie@r-a-d.io>
2013-02-15 22:28:56 +0100

    Made working alpha version of the HTTP server
    This still requires work to handle metadata properly.

--- server/server.go
+++ server/server.go
@@ -1,12 +1,15 @@
 package server
 
 import (
-	"net/http"
+    "github.com/Wessie/icecast-proxy-go/http"
+    "fmt"
+    "io"
+    "strconv"
 )
 
 
 
-func adminHandler(w http.ResponseWriter, r *http.Request, user *UserID) {
+func adminHandler(w http.ResponseWriter, r *http.Request, clientID *ClientID) {
     /* Creates admin panel access to the proxy. 
 
     There are two URLs special cased that only require source level
@@ -21,7 +24,7 @@ func mainHandler(w http.ResponseWriter, r *http.Request) {
     if something is a valid mount (and has to pass to the sourceHandler)
     or is a different valid or invalid URL. 
     */
-    
+    fmt.Println(r.URL)
     if r.Method == "SOURCE" {
         /* This is a new icecast source, pass it to the separate handler */
         makeAuthHandler(sourceHandler)(w, r)
@@ -33,7 +36,7 @@ func mainHandler(w http.ResponseWriter, r *http.Request) {
             makeAuthHandler(metadataHandler)(w, r)
         } else if path == "/admin/listclients" {
             /* A request to get the mountpoint listeners. */
-            return
+            makeAuthHandler(listclientsHandler)(w, r)
         } else if lenPath >= 6 && path[:6] == "/admin" {
             /* Admin access, pass it to the handler */
             makeAuthHandler(adminHandler)(w, r)
@@ -45,37 +48,64 @@ func mainHandler(w http.ResponseWriter, r *http.Request) {
     }
 }
 
-func errorHandler(w http.ResponseWriter, r *http.Request, error string) {
+func errorHandler(w http.ResponseWriter, r *http.Request, err string) {
     /* Returns a generic error */
     return
 }
 
+func listclientsHandler(w http.ResponseWriter, r *http.Request,
+                     clientID *ClientID) {
+
+    response := []byte("<?xml version=\"1.0\"?>\n<icestats><source mount=\"" + clientID.Mount + "\"><Listeners>0</Listeners></source></icestats>\n")
+
+    w.Header().Set("Content-Length", strconv.Itoa(len(response)))
+    w.Write(response)
+}
+
 func metadataHandler(w http.ResponseWriter, r *http.Request,
-                     user *UserID) {
+                     clientID *ClientID) {
     /* Handles a metadata request from a source. This should make sure
     an user cannot set the metadata of another users stream and even save
     metadata of inactive users */
-    return
+    fmt.Println("Sending back empty response")
+    
+    response := []byte("<?xml version=\"1.0\"?>\n<iceresponse><message>Metadata update successful</message><return>1</return></iceresponse>\n")
+    
+    w.Header().Set("Content-Length", strconv.Itoa(len(response)))
+    w.Write(response)
 }
 
-func sourceHandler(w http.ResponseWriter, r *http.Request, user *UserID) {
+func sourceHandler(w http.ResponseWriter, r *http.Request, clientID *ClientID) {
     /* Handler for icecast source requests. This can only be called by
     authenticated requests */
+    fmt.Println("New source request")
+    // Icecast clients expect a 200 OK response before sending data.
+    w.WriteHeader(http.StatusOK)
+    // Make sure to send the extra newline to signify end of headers
+    io.WriteString(w, "\r\n")
+    // And flush the data because buffering is FUN!
+    if flush, ok := w.(http.Flusher); ok {
+        flush.Flush()
+    }    
+    
+    // We can now start hijacking the connection
     hj, ok := w.(http.Hijacker)
     if !ok {
         http.Error(w, "Webserver doesn't support hijacking.",
                    http.StatusInternalServerError)
+        fmt.Println("Webserver doesn't support hijacking.")
         return
     }
     
     conn, bufrw, err := hj.Hijack()
     if err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
+        fmt.Println(err.Error())
         return
     }
     
     // Create a client struct, this is defined in client.go
-    client := NewClient(conn, bufrw, user, "")
+    client := NewClient(conn, bufrw, clientID)
     
     ClientManager.Receiver <- client
     
@@ -84,8 +114,11 @@ func sourceHandler(w http.ResponseWriter, r *http.Request, user *UserID) {
 }
 
 func Initialize() {
-	http.HandleFunc("/admin", makeAuthHandler(adminHandler))
-	http.HandleFunc("/", mainHandler)
+    // Call auth init here since it depends on some other initializions
+    Init_auth()
+    
+    http.HandleFunc("/admin", makeAuthHandler(adminHandler))
+    http.HandleFunc("/", mainHandler)
 
-	http.ListenAndServe(":8050", nil)
+    http.ListenAndServe(":8050", nil)
 }
